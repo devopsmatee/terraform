1fde80e0f003f21c875a3d238356091d7aaaaab2
devopsmatee <126463846+devopsmatee@users.noreply.github.com>
2025-07-10 13:59:18 +0300

    Update provider.tf

--- oke-cluster/provider.tf
+++ oke-cluster/provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
-  tenancy_ocid         = "ocid1.tenancy.oc1..aaaaaaaacn7t6gy654vrm25vs44g6n5o3gib53igwhrfmkv56k5j7d4mt2ea"
-  user_ocid            = "ocid1.user.oc1..aaaaaaaafycjq4t6vca3cybrs7lqlujafkxvaosbh7s2tx3gwb4ru6n4lcpq"
-  fingerprint          = "66:ec:91:27:12:13:0e:63:82:67:5d:2a:d1:1e:53:37"
+  tenancy_ocid         = "ocid1.tenancy.oc1..aaaaaaaac"
+  user_ocid            = "ocid1.user.oc1..aaaaaaaa"
+  fingerprint          = "66:ec:91"
   private_key_path     = "/root/.oci/bm.pem"
   region               = "me-riyadh-1"
 }
